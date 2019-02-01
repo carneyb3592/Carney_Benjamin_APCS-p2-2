436ad1013e2b630ca7709bf093b99e85c58f37a3
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-02-01 10:42:30 -0800

    Added Unit 2 files

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,16 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double base = base1 + base2;
+		double finalBase = base/2;
+		
+		return finalBase * height;
+	}
+}
