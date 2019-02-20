aac1948fdb336803ad79cc7bf0e483f6b98d4334
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-02-20 10:22:08 -0800

    Finished Triples

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -7,10 +7,7 @@ import static java.lang.System.*;
 public class Triples
 {
    private int number;
-   private int a2;
-   private int b2;
-   private int c2;
-
+   
 	public Triples()
 	{
 		this(0);
@@ -27,49 +24,50 @@ public class Triples
 		number = num;
 		
 	}
-	
-	private int greatestCommonFactor(int a, int b, int c)
+	public String findTriple()
 	{
-		for(int n = 0;n<= number; n++)
-		{
+		String output = "";
 		int max = number;
-		for(a = 1; a <= max; a++ ) {
-			a=n;
-			for(b = a+1; b <= max; b++ ) {
-				b=n;
-				for(c = b+1; c <= max; c++ ) {
-					c=n;
+		
+		for(int a = 1; a <= max; a++ ) {
+			
+			for(int b = a+1; b <= max; b++ ) {
+				
+				for(int c = b+1; c <= max; c++ ) {
+					
 					if(Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c,2)) 
 					{
 						if((a%2 == 0 && b%2 >0 && c%2 >0) || (a%2 > 0 && b%2 == 0 && c%2 >0))
 						{
-							int limit = Math.min(a, b);
-						    limit = Math.min(limit, c);
-						    for(int h = limit; h >= 2; h--)
-						    {
-						        if ( (a % h == 0) && (b % h == 0) && (c % h == 0) ) {
-						            a2 = a;
-						            b2 = b;
-						            c2 = c;
-						            return h;
-						        }
-						    }
-						    
+							if(greatestCommonFactor(a,b,c) == 1)
+							{ 
+								output += a + " " + b + " " + c + "\n";
+							}
 						}
-					}
-					
-						
 					}
 				}
 			}
 		}
-		return 1;
-		}
+		
+		return output;
+	}
 	
+	private int greatestCommonFactor(int a, int b, int c)
+	{
+		int limit = Math.min(a, b);
+	    limit = Math.min(limit, c);
+	    for(int h = limit; h >= 0; h--)
+	    {
+	        if ( (a % h == 0) && (b % h == 0) && (c % h == 0) ) {
+	            return h;
+	        }
+	    }
+	    return 1;
+	}	
 
 	public String toString()
 	{
-		String output=a2 + " " + b2 + " " + c2;
+		String output= "" + findTriple()+ "\n";
 		return output+"\n";
 	}
 }
