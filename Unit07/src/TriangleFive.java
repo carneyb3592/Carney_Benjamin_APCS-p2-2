df48616be72f91125b3c3425ecc98ee5e9e63ff1
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-02-20 11:09:53 -0800

    Unit 7 files

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -11,23 +11,46 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		letter = ' ';
+		amount = 0;
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
-
+	public String Triangle() {
+		String output = "";
+		for(int i = 0; i< amount;i++){
+			for(int p = 0; p< amount;p++){
+				for(int y = 0; y< amount;y++)
+				{
+					output+=letter;
+					
+				}
+				output+= ' ';
+				output+= (char)(letter+1);
+			}
+			
+		}
+		
+		
+		return output;
+	}
 	public String toString()
 	{
-		String output="";
+		String output=Triangle();
 		return output;
 	}
 }
