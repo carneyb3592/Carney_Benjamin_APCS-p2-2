964152bb5633b1aa14bad163df6ad27903b95a68
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-03-22 11:09:55 -0700

    Unit 14 files

---
+++ Unit14/src/MatrixCount1Runner.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class MatrixCount1Runner
+{
+	public static void main( String args[] ) throws Exception
+	{
+		MatrixCount1 m = new MatrixCount1();
+		System.out.println("The 7 count is :: " + m.count(7));
+	}
+}
+
+
+
