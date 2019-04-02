b2a2833490eb2e209c3c57e27f7eef56d9899bf8
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-04-02 10:56:06 -0700

    Unit 13 files

--- Unit13/src/WordSortTwo.java
+++ Unit13/src/WordSortTwo.java
@@ -12,15 +12,29 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		Scanner scan = new Scanner(sentence);
+		Scanner scan2 = new Scanner(sentence);
+		int index = 0;
+		int count = 0;
+		while(scan.hasNext()) {
+			count++;
+		}
+		wordRay = new String[count];
+		while(scan2.hasNext()) {
+			wordRay[index] = scan.nextLine();
+			index++;
+		}
 	}
 
 	public void sort()
 	{
+		
+		
 	}
 
 	public String toString()
 	{
-		String output="";
+		String output = wordRay.toString();
 		return output+"\n\n";
 	}
 }
