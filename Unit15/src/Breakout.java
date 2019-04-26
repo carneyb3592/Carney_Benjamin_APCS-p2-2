81eb5d80e141146ae78be02624749075d1a16b96
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-04-26 11:12:17 -0700

    More paddle extension

--- Unit15/src/Breakout.java
+++ Unit15/src/Breakout.java
@@ -15,7 +15,6 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 	private Ball ball;
 	private Brick brick;
 	private Paddle leftPaddle;
-	private Paddle rightPaddle;
 	private boolean[] keys;
 	private BufferedImage back;
 	private int leftScore=0;
@@ -25,7 +24,7 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		//set up all variables related to the game
 		brick = new Brick(0,0,50,50,Color.GREEN,1);
 		ball = new Ball(200,200,10,10,1,1);
-		leftPaddle = new Paddle(20,300,40,40,Color.black,speed);
+		leftPaddle = new Paddle(0,0,40,40,Color.black,speed);
 		keys = new boolean[4];
 		ball.setColor(Color.red);
     
@@ -41,10 +40,11 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 	  
    }
    public void restart() {
-	    ball = new Ball(200,200,10,10,1,1);
-		leftPaddle = new Paddle(20,300,10,50,Color.black,speed);
-		keys = new boolean[4];
-		ball.setColor(Color.red);
+	   brick = new Brick(0,0,50,50,Color.GREEN,1);
+	   ball = new Ball(200,200,10,10,1,1);
+	   leftPaddle = new Paddle(20,300,10,50,Color.black,speed);
+	   keys = new boolean[4];
+	   ball.setColor(Color.red);
    
 	
 		
@@ -70,7 +70,6 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		graphToBack.drawString("Left Score: " + leftScore, 10, 10);
 		ball.moveAndDraw(graphToBack);
 		leftPaddle.draw(graphToBack);
-		rightPaddle.draw(graphToBack);
 
 
 		//see if ball hits left wall or right wall
@@ -82,7 +81,6 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 				
 			}
 			if(ball.getX() >= 780) {
-				leftScore+=1;
 				ball.setXSpeed(0);
 				ball.setYSpeed(0);
 				
@@ -92,12 +90,16 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		}
 		//see if the ball hits the top or bottom wall 
 		if(!(ball.getY()>=10 && ball.getY()<= 550)){
-			ball.setYSpeed(-ball.getYSpeed());
+			ball.setXSpeed(0);
+			ball.setYSpeed(0);
+			restart();
 			
 		}
 	//see if the ball hits the left paddle
-		if(ball.getX() == leftPaddle.getX() + leftPaddle.getWidth() && ((leftPaddle.getY() <= ball.getY()) && (ball.getY() <= leftPaddle.getY() + leftPaddle.getHeight()))){
-			//ball.increaseSpeed(ball.getXSpeed());
+		if(ball.getX() == leftPaddle.getX() + leftPaddle.getWidth() && 
+				((leftPaddle.getY() <= ball.getY()) && 
+				(ball.getY() <= leftPaddle.getY() 
+				+ leftPaddle.getHeight()))){
 
 			ball.setXSpeed(-ball.getXSpeed());
 			
@@ -105,9 +107,10 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		
 		
 		//see if the ball hits the right paddle
-		if ((ball.getX() == rightPaddle.getX() - rightPaddle.getWidth()) && ((rightPaddle.getY() <= ball.getY()) && (ball.getY() <= rightPaddle.getY() + rightPaddle.getHeight())))
+		if ((ball.getX() == leftPaddle.getX() - leftPaddle.getWidth()) && 
+				((leftPaddle.getY() <= ball.getY()) && 
+						(ball.getY() <= leftPaddle.getY() + leftPaddle.getHeight())))
 	        {
-				//ball.increaseSpeed(ball.getXSpeed());
 
 	        	ball.setXSpeed(-ball.getXSpeed());
 
@@ -124,11 +127,11 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		}
 		if(keys[2] == true)
 		{
-			rightPaddle.moveDownAndDraw(window);
+			leftPaddle.moveLeftAndDraw(window);
 		}
 		if(keys[3] == true)
 		{
-			rightPaddle.moveUpAndDraw(window);
+			leftPaddle.moveRightAndDraw(window);
 		}
 
 		twoDGraph.drawImage(back, null, 0, 0);
@@ -140,8 +143,8 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		{
 			case 'W' : keys[0]=true; break;
 			case 'S' : keys[1]=true; break;
-			case 'I' : keys[2]=true; break;
-			case 'K' : keys[3]=true; break;
+			case 'A' : keys[2]=true; break;
+			case 'D' : keys[3]=true; break;
 		}
 	}
 
@@ -149,10 +152,10 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 	{
 		switch(toUpperCase(e.getKeyChar()))
 		{
-			case 'W' : keys[0]=false; break;
-			case 'S' : keys[1]=false; break;
-			case 'I' : keys[2]=false; break;
-			case 'K' : keys[3]=false; break;
+		case 'W' : keys[0]=true; break;
+		case 'S' : keys[1]=true; break;
+		case 'A' : keys[2]=true; break;
+		case 'D' : keys[3]=true; break;
 		}
 	}
 
