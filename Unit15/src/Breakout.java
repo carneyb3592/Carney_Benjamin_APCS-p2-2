010f08434b3ab6e9d3933a417ae8218c59615917
carneyb3592 <47190012+carneyb3592@users.noreply.github.com>
2019-04-30 11:11:05 -0700

    Breakout updates

--- Unit15/src/Breakout.java
+++ Unit15/src/Breakout.java
@@ -19,11 +19,13 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 	private BufferedImage back;
 	private int leftScore=0;
 	private int speed = 2;
+	private int counter = 0;
+	private int prevPaddlePos;
 	public Breakout()
 	{
 		//set up all variables related to the game
 		brick = new Brick(0,0,50,50,Color.GREEN,1);
-		ball = new Ball(200,200,10,10,1,1);
+		ball = new Ball(200,200,10,10,-1,-1);
 		leftPaddle = new Paddle(0,0,40,40,Color.black,speed);
 		keys = new boolean[4];
 		ball.setColor(Color.red);
@@ -41,8 +43,8 @@ public class Breakout extends Canvas implements KeyListener, Runnable
    }
    public void restart() {
 	   brick = new Brick(0,0,50,50,Color.GREEN,1);
-	   ball = new Ball(200,200,10,10,1,1);
-	   leftPaddle = new Paddle(20,300,10,50,Color.black,speed);
+	   ball = new Ball(200,200,10,10,-1,-1);
+		leftPaddle = new Paddle(0,0,40,40,Color.black,speed);
 	   keys = new boolean[4];
 	   ball.setColor(Color.red);
    
@@ -71,48 +73,64 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		ball.moveAndDraw(graphToBack);
 		leftPaddle.draw(graphToBack);
 
-
-		//see if ball hits left wall or right wall
+		//see if ball hits left wall or right wall or up and down wall
 		if(!(ball.getX()>=10 && ball.getX()<=780)){
-			if(ball.getX()<= 10) {
+			if(ball.getX()<= 0) {
 				
-				ball.setXSpeed(0);
-				ball.setYSpeed(0);
+				ball.setXSpeed(-ball.getXSpeed());
+			}
+			if(ball.getX() >= 760) {
+				ball.setXSpeed(-ball.getXSpeed());
+				
 				
 			}
-			if(ball.getX() >= 780) {
-				ball.setXSpeed(0);
-				ball.setYSpeed(0);
-				
-			}
-			restart();
 			
 		}
 		//see if the ball hits the top or bottom wall 
 		if(!(ball.getY()>=10 && ball.getY()<= 550)){
-			ball.setXSpeed(0);
-			ball.setYSpeed(0);
-			restart();
+			ball.setYSpeed(-ball.getYSpeed());
+			//restart();
 			
 		}
-	//see if the ball hits the left paddle
+	//see if the ball hits the right side
 		if(ball.getX() == leftPaddle.getX() + leftPaddle.getWidth() && 
 				((leftPaddle.getY() <= ball.getY()) && 
 				(ball.getY() <= leftPaddle.getY() 
-				+ leftPaddle.getHeight()))){
-
+				+ leftPaddle.getHeight())))
+		{
+			ball.setX(ball.getX() + ball.getWidth());
 			ball.setXSpeed(-ball.getXSpeed());
-			
 		}
-		
-		
-		//see if the ball hits the right paddle
-		if ((ball.getX() == leftPaddle.getX() - leftPaddle.getWidth()) && 
+		//see if the ball hits the left side
+		if (ball.getX() + ball.getWidth()== leftPaddle.getX()  && 
 				((leftPaddle.getY() <= ball.getY()) && 
 						(ball.getY() <= leftPaddle.getY() + leftPaddle.getHeight())))
 	        {
-
-	        	ball.setXSpeed(-ball.getXSpeed());
+			ball.setX(ball.getX() - ball.getWidth());	
+			ball.setXSpeed(-ball.getXSpeed());
+	        	/**System.out.println(true);
+	        	System.out.println(leftPaddle.getX() - leftPaddle.getWidth() );
+	        	System.out.println(leftPaddle.getX());
+	        	System.out.println(ball.getX());*/
+	        	
+	        }
+		//see if ball hits bottom
+		if(ball.getY() == leftPaddle.getY() + leftPaddle.getHeight() && 
+				((leftPaddle.getX() <= ball.getX()) && 
+				(ball.getX() <= leftPaddle.getX() 
+				+ leftPaddle.getWidth())))
+		{
+			ball.setY(ball.getY() + ball.getHeight());
+			ball.setYSpeed(-ball.getYSpeed());
+		}
+		//see if ball hits top
+		if ((ball.getY()  + ball.getHeight() == leftPaddle.getY()) && 
+				((leftPaddle.getX() <= ball.getX()) && 
+						(ball.getX() <= leftPaddle.getX() + leftPaddle.getWidth())))
+	        {
+	        	ball.setY(ball.getY() - ball.getHeight());	
+				ball.setYSpeed(-ball.getYSpeed());
+	        	//System.out.println(true);
 
 	        }
 		//see if the paddles need to be moved
@@ -135,6 +153,7 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 		}
 
 		twoDGraph.drawImage(back, null, 0, 0);
+		prevPaddlePos = leftPaddle.getX();
 	}
 
 	public void keyPressed(KeyEvent e)
@@ -152,10 +171,10 @@ public class Breakout extends Canvas implements KeyListener, Runnable
 	{
 		switch(toUpperCase(e.getKeyChar()))
 		{
-		case 'W' : keys[0]=true; break;
-		case 'S' : keys[1]=true; break;
-		case 'A' : keys[2]=true; break;
-		case 'D' : keys[3]=true; break;
+		case 'W' : keys[0]=false; break;
+		case 'S' : keys[1]=false; break;
+		case 'A' : keys[2]=false; break;
+		case 'D' : keys[3]=false; break;
 		}
 	}
 
